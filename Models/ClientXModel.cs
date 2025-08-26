f9bfe66f03bd805f16f5417b73140d412e5996ed
sahandrbp <124429732+sahandrbp@users.noreply.github.com>
2025-08-26 23:01:14 +0430

    Add project files.

---
+++ Models/ClientXModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatBot.Models
+{
+    public class ClientXModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}
